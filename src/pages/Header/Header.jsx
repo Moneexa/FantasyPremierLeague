777b1e6472635e6b556c1e9bb4799b4d07e93a29
Moneeza Syed <14besemsyed@seecs.edu.pk>
2020-12-19 22:59:13 +0500

    changed styles

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import { NavLink, BrowserRouter as Router } from 'react-router-dom'
 import { Nav, Navbar } from 'react-bootstrap'
+import { useStoreActions, useStoreState } from 'easy-peasy';
+
 const styledDiv = {
     "backgroundImage": 'url("Hero.png")',
 
@@ -9,20 +11,13 @@ const styledDiv = {
     backgroundPosition: "center center",
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
+    zIndex: "1"
     // backgroundAttachment: "fixed"
 
 
 }
 const styledButton = {
-    "borderRadius": "26px",
-    color: "#492477",
-    width: "234px",
-    padding: "0.2rem",
-    fontSize: "20px",
-    fontWeight: "500",
-    marginTop: "1rem",
-    marginLeft:"4rem",
-    border:"none"
+    
     // "width": "100%"
 
 
@@ -39,74 +34,74 @@ const styledNav = {
 }
 const styledHeading = {
     "color": "white",
-    "marginLeft" :"4rem"
+    "marginLeft": "4rem"
 
 }
 export default function Header() {
+    const heading = useStoreState(state => state.obj.heading)
     return (
         <>
             <div className="d-flex flex-column Header" id="Header" style={styledDiv}>
-                <Router>
-                    <Navbar bg="transparent" variant="dark" expand="lg" className="d-flex" style={styledNav}>
-                        <Navbar.Brand href="/" className="">
-                            <img src="logo.png" style={styledLogo} />
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav" className=" flex-grow-0">
-                            <Nav className="ml-auto" style={{
-                                fontSize: "20px",
-                                fontWeight: "500",
-                                color: "white"
-                                // "marginRight":"8rem"
+                <Navbar bg="transparent" variant="dark" expand="lg" className="d-flex" style={styledNav}>
+                    <Navbar.Brand href="/home" className="">
+                        <img src="logo.png" style={styledLogo} />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" className=" flex-grow-0">
+                        <Nav className="ml-auto" style={{
+                            fontSize: "20px",
+                            fontWeight: "500",
+                            color: "white"
+                            // "marginRight":"8rem"
 
+
+                        }}>
+                            <Nav.Link as={NavLink} className="color-white pl-5" to="/home" style={{
+                                color: "white"
+                            }}>
+                                Home
+                            </Nav.Link>
+                            <Nav.Link className="color-white pl-5" to="/how-to-play" style={{
+                                color: "white"
 
                             }}>
-                                <Nav.Link as={NavLink} className="color-white pl-5" to="/home" style={{
-                                    color: "white"
-                                }}>
-                                    Home
+                                HowToPlay
                             </Nav.Link>
-                                <Nav.Link as={NavLink} className="color-white pl-5" to="/how-to-play" style={{
-                                    color: "white"
+                            <Nav.Link as={NavLink} to="/faq" className="color-white pl-5" style={{
+                                color: "white"
 
-                                }}>
-                                    HowToPlay
+                            }}>
+                                FAQs
                             </Nav.Link>
-                                <Nav.Link as={NavLink} to="/faq" className="color-white pl-5" style={{
-                                    color: "white"
+                            <Nav.Link as={NavLink} to="/signin" className="color-white pl-5" style={{
+                                color: "white"
+
+                            }}>
+                                <button style={{
+                                    "borderRadius": "26px",
+                                    padding: "0.25rem 2.5rem 0.25rem 2.5rem",
+                                    fontSize: "20px",
+                                    fontWeight: "500",
+                                    color: "#492477",
+                                    border: "none"
 
                                 }}>
-                                    FAQs
-                            </Nav.Link>
-                                <Nav.Link as={NavLink} to="/login" className="color-white pl-5" style={{
-                                    color: "white"
-
-                                }}>
-                                    <button  style={{
-                                        width: "8rem", "borderRadius": "26px",
-                                        padding: "0.25rem",
-                                        fontSize: "20px",
-                                        fontWeight: "500",
-                                        color: "#492477",
-                                        border:"none"
-
-                                    }}>
-                                        Sign In
+                                    Sign In
                                     </button>
-                                </Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
 
-                    </Navbar>
-                </Router>
-                <div className="d-flex justify-content-center row" style={{ marginTop: "4rem", width:"100%"}}>
+                </Navbar>
+                <div className="d-flex justify-content-center row heading-row">
                     <div className="container">
-                        <h1
-                            style={styledHeading}>
-                            Win the Game, <br />
-                            Win the Day.
+                        <h1 className="heading-h1"
+                            >
+                            {heading}
                         </h1>
-                        <button style={styledButton}>Start Playing</button>
+                        {heading !== "Win the Game, Win the Day." || heading === "" ? "" :
+                            <button className="start-playing">Start Playing</button>
+                        }
 
                     </div>
                 </div>
