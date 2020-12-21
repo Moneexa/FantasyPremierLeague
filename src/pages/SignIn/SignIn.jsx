38f7e9b61203e192106e9fc68a5b64a849729c6b
Moneeza Syed <14besemsyed@seecs.edu.pk>
2020-12-21 12:13:05 +0500

    minor responsiveness

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { useStoreState, useStoreActions } from 'easy-peasy'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShieldAlt, faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
 import './SignIn.css'
-export default function SignIn() {
+export default function SignIn({ match }) {
     const heading = useStoreState(state => state.obj.heading)
     const changeHeading = useStoreActions(actions => actions.obj.changeHeadingRed)
     const loggedIn = useStoreActions(actions => actions.obj.changeLoggingRed)
@@ -12,19 +12,14 @@ export default function SignIn() {
     const [password, setPassword] = useState("")
     useEffect(() => {
         changeHeading("")
+        console.log(match.path)
 
     }, [])
 
-    useEffect(() => {
-        document.getElementById('Header').style.background = "linear-gradient(to left, #B90D8E, #110A3E)"
-        document.getElementById('Header').style.borderRadius = "0px 0px 500px 500px"
-    })
     function handleEmailChange(e) { }
     function handlePasswordChange(e) { }
     function signin() {
         loggedIn(true)
-        document.getElementById('Header').style.background = "url('Hero.png') linear-gradient(0deg, transparent, transparent)"
-        document.getElementById('Header').style.borderRadius = "0px 0px 500px 500px"
     }
     return (<>
         <div className="d-flex flex-column justify-content-center align-items-center signin-form">
@@ -32,7 +27,7 @@ export default function SignIn() {
             <p className="py-3 px-2 para">
                 You are just a step away to enter the gaming experience, Sign in with your FPL account and get started
             </p>
-            <form className="d-flex flex-column align-items-center justify-content-center py-2" style={{ margin: "-1rem" }}>
+            <form className="d-flex flex-column align-items-center justify-content-center " style={{ margin: "-1rem" }}>
                 <input value={email} onChange={(e) => handleEmailChange(e)} className="fpl-email" placeholder="Your FPL email" />
                 <input className="fpl-email" value={password} onChange={(e) => handlePasswordChange(e)}
                     placeholder="Your FPL password" />
@@ -42,14 +37,14 @@ export default function SignIn() {
             </form>
             <div className="d-flex align-items-center justify-content-center last-para" >
                 <div className="mr-1 d-flex align-items-center justify-content-center" style={{ width: "43%" }}>
-                    <FontAwesomeIcon className="mb-1" icon={faQuestionCircle} style={{font: "normal normal medium 25px Rubik", color: "white" }} />
+                    <FontAwesomeIcon className="mb-1" icon={faQuestionCircle} style={{ font: "normal normal medium 25px Rubik", color: "white" }} />
                     <p style={{
                         font: "normal normal 300 0.8rem Rubik"
                     }}> Why we need your FPL credentials?
                     </p>
                 </div>
                 <div className="ml-1 d-flex align-items-center justify-content-center" style={{ width: "32%" }}>
-                    <FontAwesomeIcon icon={faShieldAlt} className="mb-1" style={{ font: "normal normal medium 25px Rubik",color: "white" }} />
+                    <FontAwesomeIcon icon={faShieldAlt} className="mb-1" style={{ font: "normal normal medium 25px Rubik", color: "white" }} />
                     < p style={{
                         font: "normal normal 300 0.8rem Rubik"
                     }}>Completely Secure Find out how</p>
