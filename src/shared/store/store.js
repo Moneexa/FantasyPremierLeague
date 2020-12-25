9c5e8d8a8927ad9938054d1191fd028bb241c9af
Moneeza Syed <14besemsyed@seecs.edu.pk>
2020-12-25 11:29:45 +0500

    added leagues

--- src/shared/store/store.js
+++ src/shared/store/store.js
@@ -30,80 +30,124 @@ export const store = createStore({
         },
         ],
         para: "",
-        myLeagues:[
-          {
-              id:"1",
-              gameWeakLeague:"Game Weak League 10",
-              leagueNo:"2171",
-              yourRank:"1337",
-              stake:"$100"
-          },
-          {
-            id:"2",
-            gameWeakLeague:"Game Weak League 09",
-            leagueNo:"1927",
-            yourRank:"2365",
-            stake:"$100"
+        myLeagues: [
+            {
+                id: "1",
+                gameWeakLeague: "Game Weak 10",
+                leagueNo: "2171",
+                yourRank: "1337",
+                stake: "$100"
+            },
+            {
+                id: "2",
+                gameWeakLeague: "Game Weak 09",
+                leagueNo: "1927",
+                yourRank: "2365",
+                stake: "$100"
 
-        },
-        {
-            id:"3",
-            gameWeakLeague:"Game Weak League 08",
-            leagueNo:"1246",
-            yourRank:"1985",
-            stake:"$100"
+            },
+            {
+                id: "3",
+                gameWeakLeague: "Game Weak 08",
+                leagueNo: "1246",
+                yourRank: "1985",
+                stake: "$100"
 
-        },
+            },
 
-        {
-            id:"4",
-            gameWeakLeague:"Game Weak League 07",
-            leagueNo:"2896",
-            yourRank:"5638",
-            stake:"$100"
+            {
+                id: "4",
+                gameWeakLeague: "Game Weak 07",
+                leagueNo: "2896",
+                yourRank: "5638",
+                stake: "$100"
 
-        },
+            },
 
-        {
-            id:"5",
-            gameWeakLeague:"Game Weak League 06",
-            leagueNo:"1522",
-            yourRank:"2997",
-            stake:"$100",
-        },
+            {
+                id: "5",
+                gameWeakLeague: "Game Weak 06",
+                leagueNo: "1522",
+                yourRank: "2997",
+                stake: "$100",
+            },
 
-        {
-            id:"5",
-            gameWeakLeague:"Game Weak League 06",
-            leagueNo:"1522",
-            yourRank:"2997",
-            stake:"$100",
-        },
+            {
+                id: "5",
+                gameWeakLeague: "Game Weak 06",
+                leagueNo: "1522",
+                yourRank: "2997",
+                stake: "$100",
+            },
 
-        {
-            id:"5",
-            gameWeakLeague:"Game Weak League 06",
-            leagueNo:"1522",
-            yourRank:"2997",
-            stake:"$100",
-        },
-        {
-            id:"5",
-            gameWeakLeague:"Game Weak League 06",
-            leagueNo:"1522",
-            yourRank:"2997",
-            stake:"$100",
-        },
+            {
+                id: "5",
+                gameWeakLeague: "Game Weak 06",
+                leagueNo: "1522",
+                yourRank: "2997",
+                stake: "$100",
+            },
+            {
+                id: "5",
+                gameWeakLeague: "Game Weak 06",
+                leagueNo: "1522",
+                yourRank: "2997",
+                stake: "$100",
+            },
 
 
         ],
+        paymentHistory: [
+            {
+                transType: "cardDeposit",
+                amount: "-17.5",
+                date:"18, Dec,2020",
+                currency: "USD"
 
+
+            },
+            {
+                transType: "paypalDeposit",
+                amount: "220",
+                date:"18, Dec,2020",
+                currency: "USD"
+
+            },
+            {
+                transType:"withdraw",
+                amount:"-330",
+                date:"18, Dec,2020",
+                currency: "USD"
+
+
+
+
+            },
+            {
+                transType: "cancel",
+                amount:"0",
+                date:"18, Dec,2020",
+                currency: "USD"
+
+
+            }
+        ],
+        currentBalance: 15750,
         changeHeading: action((state, payload) => {
             state.heading = payload
         }),
         changePara: action((state, payload) => {
-            state.para=payload
+            state.para = payload
 
+        }),
+        changeCurrentBalance: action((state, payload) => {
+            if (payload.type === "withdraw") {
+                state.currentBalance = state.currentBalance - payload.value
+            }
+            else {
+                state.currentBalance = state.changeCurrentBalance + payload.value
+            }
+            state.currentBalance = payload
         }),
         changeLogging: action((state, payload) => {
             state.loggedIn = payload
@@ -115,12 +159,27 @@ export const store = createStore({
         changeLoggingRed: thunk((actions, payload) => {
             actions.changeLogging(payload)
         }),
-        changeParaRed: thunk((actions, payload)=>{
+        changeParaRed: thunk((actions, payload) => {
             actions.changePara(payload)
         }),
         submitEmail: thunk((actions, payload) => {
             console.log(payload)
+        }),
+        depositMoney: thunk((actions, payload) => {
+            const obj = {
+                type: "deposit",
+                value: payload
+            }
+            actions.changeCurrentBalance(obj)
+        }),
+        withdrawMoney: thunk((actions, payload) => {
+            const obj = {
+                type: "withdraw",
+                value: payload
+            }
+            actions.changeCurrentBalance(obj)
         })
+
 
     }
 
