78ddab42ca3759d27c7d700bff28c244236c3cc1
Moneeza Syed <14besemsyed@seecs.edu.pk>
2021-01-28 23:05:15 +0500

    new stuff

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -19,6 +19,8 @@ import wave from './wave single.svg'
 export default function Home() {
 
     const loggedIn = useStoreState(state => state.obj.loggedIn);
+    const sync = useStoreState(state => state.obj.sync);
+
     const _myLeagues = useStoreState(state => state.obj.myLeagues);
     const myRefname = useRef(null);
 
@@ -46,7 +48,7 @@ export default function Home() {
             console.log(faqList ? faqList : '')
         }
     }, [])
-    function func1() { 
+    function func1() {
         setExpand4(true)
     }
     function handleChange(value) {
@@ -62,17 +64,48 @@ export default function Home() {
         else if (value === 3) {
             expandOR4 ? setExpand4(false) : setExpand4(true)
         }
-        else if(value===4){
+        else if (value === 4) {
             expandOR5 ? setExpand5(false) : setExpand5(true)
         }
     }
     return (<>
+        {
+            loggedIn && !sync ?
+                <div className="d-flex align-items-center justify-content-center position-fixed w-100" style={{
+                    top: "0px", left: "0px",
+                    background: "#DFF4FFD1",
+                    boxShadow: "0px 5px 6px #00000073",
+                    padding:"1.3rem 2.6rem"
+
+                }}>
+
+                    <div className="text-center text-black mr-3 mr-sm-2" style={{
+                        fontSize: "1rem",
+                        fontFamily: "Rubik",
+                    }}>
+                        Sync your FPL Team to start Playing
+                    </div>
+                    <Link to="/sync-fpl">
+                        <button className="text-white text-center"
+                            style={{
+                                fontSize: "0.8rem", fontFamily: "Rubik", padding: "0.46rem 1.72rem", backgroundColor: "#0F1F4E",
+                                borderRadius: "26px", border: "hidden"
+
+                            }}
+                        > Sync Now</button>
+                    </Link>
+                </div>
+                : ''
+
+
+        }
         <div className="d-flex align-items-center justify-content-center flex-wrap" id="cards">
 
             <div className="d-flex flex-column justify-content-center align-items-center mx-2" id="div1">
-                <h2 className="card-heading"> Weekly </h2>
+                <div className="card-heading"> Weekly </div>
                 <p className="pt-2" style={{
-                    font: "normal normal 300 1rem Rubik"
+                    font: "normal normal 300 1rem Rubik",
+                    marginTop: "2.25rem"
 
                 }}>Gameweek 20 League</p>
                 <a href="#faq3">
@@ -92,20 +125,22 @@ export default function Home() {
                     }}>Cash winnings for 1st , 2nd and 3rd ></p>
                 </Link>
                 {
-                    !loggedIn ? <Link to="/signin"><button className="gets-started-button">
+                    !loggedIn ? <Link to="/signin"><button className="gets-started-button bg-white">
                         Get Started
-                   </button>
-                    </Link> : <Link to="/stakes"><button className="gets-started-button">
+       </button>
+                    </Link> : <Link to="/stakes"><button className="gets-started-button bg-white">
                         Get Started
-                   </button></Link>
+       </button></Link>
                 }
 
             </div>
 
             <div className="d-flex flex-column justify-content-center align-items-center mx-2" id="div2">
-                <h2>Monthly</h2>
+                <div className="card-heading">Monthly</div>
                 <p className="pt-2" style={{
-                    font: "normal normal 300 1rem Rubik"
+                    font: "normal normal 300 1rem Rubik",
+                    marginTop: "2.25rem"
+
 
                 }}>February League</p>
                 <a href="#faq3">
@@ -125,23 +160,23 @@ export default function Home() {
                     }}>Cash winnings for 1st , 2nd and 3rd ></p>
                 </Link>
                 {
-                    !loggedIn ? <Link to="/signin"><button className="gets-started-button">
+                    !loggedIn ? <Link to="/signin"><button className="gets-started-button bg-white">
                         Get Started
-                   </button>
-                    </Link> : <Link to="/stakes"><button className="gets-started-button">
+       </button>
+                    </Link> : <Link to="/stakes"><button className="gets-started-button bg-white">
                         Get Started
-                   </button></Link>
+       </button></Link>
                 }
             </div>
         </div>
-        <div className="d-flex flex-column justify-content-center align-items-center" id="how-to-play" style={{padding:"8rem"}}>
+        <div className="d-flex flex-column justify-content-center align-items-center big-rounded-top big-rounded-bottom" id="how-to-play">
             {!loggedIn ?
                 <>
                     <h1 className="pb-3 how-to-play-heading">How to Play</h1>
                     <p className="pt-3 how-to-play-text">
                         We made it easy to play and win! Login with your Fantasy premier League account and compete with
                         players close to your Overall Rank
-                       </p>
+           </p>
                     <div className="d-flex align-items-center justify-content-center circle-row">
 
 
@@ -149,10 +184,12 @@ export default function Home() {
 
                             <div className="d-flex flex-column justify-content-center align-items-center main-circle">
                                 <div className="rounded-circle d-flex align-items-center justify-content-center circle" >
-                                    <img src={Select} />
+                                    <img src={Select} style={{ height: "4.75rem" }} />
                                 </div>
                                 <h2   >Select</h2>
-                                <p className="rounded-circle-text">Select your Monthly or Weekly league.</p>
+                                <p className="rounded-circle-text"
+                                    style={{ maxWidth: "16rem" }}
+                                >Select your Monthly or Weekly league.</p>
                             </div>
                             <div className="d-flex justify-content-center mx-n5 flex-grow-1" style={{
                                 alignSelf: "stretch",
@@ -165,10 +202,12 @@ export default function Home() {
 
                                 <div className="rounded-circle d-flex align-items-center justify-content-center circle" >
 
-                                    <img src="Choose.png" style={{ height: "13rem" }} />
+                                    <img src="Choose.png" style={{ height: "8.75rem" }} />
                                 </div>
                                 <h2>Wager</h2>
-                                <p className="rounded-circle-text" >Choose the league you want to participate in.</p>
+                                <p className="rounded-circle-text"
+                                    style={{ maxWidth: "16rem" }}
+                                >Choose the league you want to participate in.</p>
                             </div>
                             <div className="d-flex  justify-content-center mx-md-n5 mx-lg-n7 flex-grow-1" style={{
                                 alignSelf: "stretch",
@@ -177,26 +216,29 @@ export default function Home() {
                                 <img src={wave} className="stroke2" />
                             </div>
 
-                            <div className="d-flex flex-column justify-content-center align-items-center pl-3 main-circle">
+                            <div className="d-flex flex-column justify-content-center align-items-center main-circle">
 
                                 <div className="rounded-circle d-flex align-items-center justify-content-center circle" >
-                                    <img src={Enjoy} />
+                                    <img src={Enjoy} style={{ height: "4.75rem" }} />
 
                                 </div>
                                 <h2   >Enjoy</h2>
-                                <p className="rounded-circle-text">Sit back, relax &amp; keep refreshing the results page</p>
+                                <p className="rounded-circle-text"
+                                    style={{ maxWidth: "16rem" }}
+
+                                >Sit back, relax &amp; keep refreshing the results page</p>
                             </div>
                             <div></div>
                         </div>
                     </div>
                     <div className="d-flex flex-column justify-content-center align-items-center" id="faq">
                         <h1 className="how-to-play-heading">Frequently Asked Questions</h1>
-                        <div className="w-100 my-3">
+                        <div className="my-3" style={{ maxWidth: "50.74rem" }}>
                             {
                                 faqList.map((value, index) => {
                                     console.log(value)
                                     if (index < 5) {
-                                        return (<Accordion key={index} id={`faq${index}`} expanded={index === 0 ? expandOR : index === 1 ? expandOR2 : index === 2 ? expandOR3 : index === 3 ? expandOR4 : index===4? expandOR5 : false} onChange={() => handleChange(index)} className="mt-3" style={{
+                                        return (<Accordion key={index} id={`faq${index}`} expanded={index === 0 ? expandOR : index === 1 ? expandOR2 : index === 2 ? expandOR3 : index === 3 ? expandOR4 : index === 4 ? expandOR5 : false} onChange={() => handleChange(index)} className="mt-3" style={{
                                             borderRadius: "30px", boxShadow: "0px 3px 6px #00000029"
 
                                         }}>
@@ -221,9 +263,8 @@ export default function Home() {
                                                 }} >
 
                                                     {value.description.includes("click here")
-                                                    
-                                                    ? <Link to="/winnings">{value.description}</Link>
-                                                    : value.description
+                                                        ? <Link to="/winnings">{value.description}</Link>
+                                                        : value.description
                                                     }
                                                 </Typography>
                                             </AccordionDetails>
@@ -233,7 +274,7 @@ export default function Home() {
 
                         </div>
                         <Link to="/faq">
-                            <button className="mt-2 py-2 px-5">View All</button>
+                            <button className="mt-2 py-2 px-5 bg-white">View All</button>
 
                         </Link>
 
@@ -244,39 +285,48 @@ export default function Home() {
                     <h1 className="pb-3 how-to-play-heading">My Leagues</h1>
                     <p className="pt-3 how-to-play-text">
                         Your most recent leagues. To view all, current and past, click on View all
-                        </p>
-                    <div className="d-flex flex-column align-items-center tab1">
-                        <div className="d-flex align-items-center table-headings p-2 overflow-auto">
-                            <p className="head-text" style={{ width: "10%" }}>#</p>
-                            <p className="head-text" style={{ width: "30%" }}>GameWeek League</p>
-                            <p className="head-text" style={{ width: "25%" }}>League No.</p>
-                            <p className="head-text" style={{ width: "20%" }}>Your Rank</p>
-                            <p className="head-text" style={{ width: "15%" }}>Stack</p>
+            </p>
+
+                    <div id="table-leagues" style={{ overflow: 'auto' }}>
+                        <div className="d-flex flex-column align-items-center tab1 main-padding">
+                            <div className="d-flex align-items-center table-headings p-2 overflow-auto">
+                                <div className="head-text" style={{ width: "10%" }}>#</div>
+                                <div className="head-text" style={{ width: "30%" }}>GameWeek League</div>
+                                <div className="head-text" style={{ width: "25%" }}>League No.</div>
+                                <div className="head-text" style={{ width: "20%" }}>Your Rank</div>
+                                <div className="head-text" style={{ width: "15%" }}>Stack</div>
+                            </div>
+
+                            {myLeagues.map((value, index) => {
+                                if (index < 5) {
+                                    return (<div key={index} className="d-flex align-items-center rows mb-2 p-3 overflow-auto">
+                                        <div className="row-text" style={{
+                                            width: "10%",
+                                        }}>
+                                            <div className="d-flex align-items-center justify-content-center" style={{
+                                                border: "3px solid #0E335E",
+                                                borderRadius: "14px",
+                                                width: "3rem",
+                                                height: "3rem",
+                                                textAlign: 'center'
+                                            }}>{value.id}</div>
+                                        </div>
+                                        <div className="row-text" style={{ width: "30%", minWidth: "8rem" }}>{value.gameWeakLeague}</div>
+                                        <div className="row-text" style={{ width: "25%", minWidth: "5rem" }}>{value.leagueNo}</div>
+                                        <div className="row-text" style={{ width: "20%", minWidth: "3rem" }}>{value.yourRank}</div>
+                                        <div className="row-text pl-md-5 pl-sm-0" style={{ width: "18%", minWidth: "7rem" }}>{value.stake}
+                                            <Link to="/my-leagues" className="ml-md-2 ml-sm-0">
+                                                <img src={Arrow} style={{ height: "0.813rem" }} />
+                                            </Link>
+                                        </div>
+
+                                    </div>)
+                                }
+                            })}
                         </div>
-
-                        {myLeagues.map((value, index) => {
-                            if (index < 5) {
-                                return (<div key={index} className="d-flex align-items-center rows mb-2 p-3 overflow-auto">
-                                    <p className="row-text" style={{
-                                        width: "10%",
-                                        border: "3px solid #0E335E",
-                                        borderRadius: "14px", minWidth: "2rem",
-                                    }}>{value.id}</p>
-                                    <p className="row-text" style={{ width: "30%", minWidth: "8rem" }}>{value.gameWeakLeague}</p>
-                                    <p className="row-text" style={{ width: "25%", minWidth: "5rem" }}>{value.leagueNo}</p>
-                                    <p className="row-text" style={{ width: "20%", minWidth: "3rem" }}>{value.yourRank}</p>
-                                    <p className="row-text pl-md-5 pl-sm-0" style={{ width: "15%", minWidth: "7rem" }}>{value.stake}
-                                        <Link to="/my-leagues" className="ml-md-5 ml-sm-0">
-                                            <img src={Arrow} style={{ height: "0.813rem" }} />
-                                        </Link>
-                                    </p>
-
-                                </div>)
-                            }
-                        })}
                     </div>
                     <Link to="/my-leagues">
-                        <button className="mt-2 py-2 px-5" id="leaguesBtn">View All</button>
+                        <button className="mt-2 py-2 px-5 bg-white" id="leaguesBtn">View All</button>
 
                     </Link>
 
@@ -285,12 +335,9 @@ export default function Home() {
 
                 </>
             }
-
-
         </div>
-        <SubscribeLetter style={{ marginTop: "0px" }} />
+        <SubscribeLetter style={{ marginTop: "-6rem" }} />
 
-    </>
 
-    )
+    </>)
 }
