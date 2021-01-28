73b74b7ffbe36332ca0058b8ecdf9d6b16e550c2
Moneeza Syed <14besemsyed@seecs.edu.pk>
2021-01-28 23:04:29 +0500

    new stuff

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -17,12 +17,21 @@ import { Nav, Navbar } from 'react-bootstrap'
 import { useStoreActions, useStoreState } from 'easy-peasy';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import Profile from '../../headings/Profile'
+import CreateAccount from '../../headings/CreateAccount'
 import MenuIcon from './menu icon.svg'
+import SignIn2 from '../../headings/SignIn2'
+import Messages from '../../headings/Messages'
+import SuccessMessage from '../../headings/SuccessMessage'
+import FantasyReport from '../../headings/FantasyReport'
+import Settings from '../../headings/Settings'
+import MyWinnings from '../../headings/MyWinnings'
 const styledDiv = {
-    "backgroundImage": 'url("Hero.png")',
-    backgroundPosition: "center center",
+    "backgroundImage": 'url("Hero.jpg")',
     backgroundRepeat: "no-repeat",
+    backgroundPosition: 'right',
     backgroundSize: "cover",
+    // borderRadius: '0 0 22rem 22rem',
     zIndex: "1"
 }
 const styledButton = {
@@ -39,7 +48,7 @@ const styledLogo = {
 }
 const styledNav = {
     "marginTop": "2rem",
-    "justifyContent": "space-around"
+    "justifyContent": "space-between"
 }
 const styledHeading = {
     "color": "white",
@@ -55,11 +64,9 @@ export default function Header() {
     const loggedIn = useStoreState(state => state.obj.loggedIn)
 
     useEffect(() => {
-        if (location.pathname === "/signin")
+        if (location.pathname === "/signin" || location.pathname === "/create-account" || location.pathname === "/sync-fpl")
             setChangedStyle({
                 background: "linear-gradient(to left, #B90D8E, #110A3E)",
-                borderRadius: "0px 0px 500px 500px",
-                minHeight: "100vh",
                 zIndex: "1"
 
 
@@ -73,17 +80,19 @@ export default function Header() {
                 backgroundPosition: "center center",
                 backgroundRepeat: "no-repeat",
                 backgroundSize: "cover",
+                borderRadius: '0',
                 zIndex: "1"
             })
         }
         else if (location.pathname === "/wallet" || location.pathname === "/privacy-policy" || location.pathname === "/winnings"
+            || location.pathname === "/profile" || location.pathname === "/messages" || location.pathname === "/tracker"
+            || location.pathname === "/success" || location.pathname === "/settings" || location.pathname === "/my-winnings"
 
             || location.pathname === "/blank"
         ) {
             setChangedStyle({
                 zIndex: "1",
                 background: "linear-gradient(to left, #B90D8E, #110A3E)",
-                borderRadius: "0px 0px 500px 500px",
 
             })
         }
@@ -91,7 +100,6 @@ export default function Header() {
             setChangedStyle({
                 zIndex: "1",
                 background: "linear-gradient(to left, #B90D8E, #110A3E)",
-                borderRadius: "0px 0px 500px 500px",
 
             })
         }
@@ -101,8 +109,8 @@ export default function Header() {
     }, [location])
     return (
         <>
-            <div className="d-flex flex-column Header" id="Header" style={changedStyle}>
-                <Navbar bg="transparent" variant="dark" expand="lg" className="d-flex" style={styledNav}>
+            <div className="d-flex flex-column Header big-rounded-bottom main-padding" id="Header" style={changedStyle}>
+                <Navbar bg="transparent" variant="dark" expand="lg" className="d-flex px-0" style={styledNav}>
                     <Navbar.Brand href="/home" className="">
                         <img src="logo.png" style={styledLogo} />
                     </Navbar.Brand>
@@ -111,7 +119,8 @@ export default function Header() {
                         {
                             location.pathname === "/menu" ?
                                 <Nav className="ml-auto" style={{
-                                    fontSize: "20px",
+                                    fontSize: "16px",
+                                    fontFamily: "Rubik",
                                     fontWeight: "500",
                                     color: "white"
                                     // "marginRight":"8rem"
@@ -128,8 +137,9 @@ export default function Header() {
                                 </Nav> :
 
                                 <Nav className="ml-auto" style={{
-                                    fontSize: "20px",
+                                    fontSize: "16px",
                                     fontWeight: "500",
+                                    fontFamily: "Rubik",
                                     color: "white"
                                     // "marginRight":"8rem"
 
@@ -161,8 +171,18 @@ export default function Header() {
                                             color: "white"
 
                                         }}>
-                                            MyLeagues
+                                            My Leagues
                             </Nav.Link>
+                                    }
+                                    {
+                                        loggedIn ?
+                                            <Nav.Link as={NavLink} to="/messages" className="color-white pl-5" style={{
+                                                color: "white"
+
+                                            }}>
+                                                Messages
+                                            </Nav.Link> :
+                                            ''
                                     }
                                     {
                                         !loggedIn ? <Nav.Link as={NavLink} to="/signin" className="color-white pl-5" style={{
@@ -173,8 +193,9 @@ export default function Header() {
 
                                             <button className="bg-white" style={{
                                                 "borderRadius": "26px",
-                                                padding: "0.25rem 2.5rem 0.25rem 2.5rem",
-                                                fontSize: "20px",
+                                                padding: "0.2rem 2.75rem",
+                                                fontSize: "16px",
+                                                fontFamily: "Rubik",
                                                 fontWeight: "500",
                                                 color: "#492477",
                                                 border: "none"
@@ -191,8 +212,9 @@ export default function Header() {
 
                                                 <button className="bg-white" style={{
                                                     "borderRadius": "26px",
-                                                    padding: "0.25rem 2.5rem 0.25rem 2.5rem",
-                                                    fontSize: "20px",
+                                                    padding: "0.1rem 1.5rem",
+                                                    fontSize: "16px",
+                                                    fontFamily: "Rubik",
                                                     fontWeight: "500",
                                                     color: "#492477",
                                                     border: "none"
@@ -202,42 +224,62 @@ export default function Header() {
                                     </button>
                                             </Nav.Link>
                                     }
+
                                     {
                                         loggedIn ?
                                             <Nav.Link as={NavLink} to="/menu" className="color-white pl-5" style={{
                                                 color: "white"
 
                                             }}><img src={MenuIcon} style={{
-                                                height: "1.5rem",
+                                                height: "1.2rem",
                                                 filter: "invert(96%) sepia(0%) saturate(0%) hue-rotate(111deg) brightness(104%) contrast(105%)"
-                                            }} /></Nav.Link> : ''
+                                            }} /></Nav.Link> :
+                                            ''
                                     }
+
+
                                 </Nav>
                         }
                     </Navbar.Collapse>
 
                 </Navbar>
                 <Switch>
-                    <Route component={SignIn} path="/signin" />
-                    <Route component={home} path="/home" />
-                    <Route component={PrivacyPolicy} path="/privacy-policy" />
-                    <Route component={Faq} path="/faq" />
-                    <Route component={MyLeagues} path="/my-leagues" />
-                    <Route component={Mayhem} path="/mayhem" />
-                    <Route path="/mayhem-monthly" component={MayhemMonthly} />
-
-                    <Route component={Wallet} path="/wallet" />
-                    <Route component={Winnings} path="/winnings" />
-                    <Route path="/stakes" component={Stakes} />
-                    <Route path="/pay-stake" component={PayStake} />
-                    <Route path="/confirm" component={Confirmation} />
+                <Route path="/create-account" component={CreateAccount} />
 
                 </Switch>
 
+                {
+                    loggedIn ?
+                        <Switch>
+                            <Route component={SignIn} path="/sync-fpl" />
+                            <Route component={PrivacyPolicy} path="/privacy-policy" />
+                            <Route component={home} path="/home" />
 
-
+                            <Route component={Faq} path="/faq" />
+                            <Route component={MyLeagues} path="/my-leagues" />
+                            <Route component={Mayhem} path="/mayhem" />
+                            <Route path="/mayhem-monthly" component={MayhemMonthly} />
+                            <Route path="/profile" component={Profile} />
+                            <Route component={Wallet} path="/wallet" />
+                            <Route component={Winnings} path="/winnings" />
+                            <Route path="/stakes" component={Stakes} />
+                            <Route path="/pay-stake" component={PayStake} />
+                            <Route path="/confirm" component={Confirmation} />
+                            <Route path="/messages" component={Messages} />
+                            <Route path="/success" component={SuccessMessage} />
+                            <Route path="/tracker" component={FantasyReport} />
+                            <Route path="/settings" component={Settings} />
+                            <Route path="/my-winnings" component={MyWinnings} />
+                        </Switch>
+                        :
+                        <Switch>
+                            <Route component={SignIn2} path="/signin" />
+                            <Route component={home} path="/home" />
+                            <Route component={PrivacyPolicy} path="/privacy-policy" />
+                            <Route component={Faq} path="/faq" />
+                        </Switch>
+                }
             </div>
-
         </>
     )
 }
