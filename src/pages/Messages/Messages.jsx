941e38268861672875f9a50c58476b9b75a631ee
Moneeza Syed <14besemsyed@seecs.edu.pk>
2021-02-13 00:02:37 +0500

    new changes

--- src/pages/Messages/Messages.jsx
+++ src/pages/Messages/Messages.jsx
@@ -51,18 +51,21 @@ export default function Messages() {
             </div>
             <div className="main-cont w-100" style={{ marginTop: "5rem" }}>
                 <div className="d-flex flex-column align-items-center justify-content-center corner-card">
-                    <InputGroup className="my-5 search-messages" style={{
+                    <InputGroup className="my-5 search-messages overflow-hidden" style={{
                         marginLeft: "6.34rem",
                         marginRight: "5.69rem",
                         marginBottom: "3.7rem"
                     }}>
-                        <InputGroup.Prepend>
+                        <InputGroup.Prepend style={{backgroundColor:"transparent", border:"hidden"}}>
                             <InputGroup.Text id="basic-addon1">
                                 <img src={search} style={{ height: "1.02rem" }} />
                             </InputGroup.Text>
                         </InputGroup.Prepend>
                         <FormControl
-                            placeholder="Search Messages" className="search-messages" />
+                            placeholder="Search Messages" className="text-white text-left input-text-bar" style={{border:"hidden",
+                            backgroundColor:"transparent"
+                            
+                            }} />
                     </InputGroup>
                     <div className="d-flex flex-column justify-content-center messages-div w-100">
 
@@ -157,7 +160,7 @@ export default function Messages() {
                                         <div className="messages text-left text-black">
                                             Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen piece.
                                         </div>
-                                        <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-3">
+                                        <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-5">
                                             <div>10:30</div>
                                             <div>12 January, 2021</div>
                                         </div>
@@ -167,7 +170,7 @@ export default function Messages() {
                                     <div className="d-flex align-items-center justify-content-center mt-5">
                                         <div className="messages text-left text-black">
                                             Lorem Ipsum is simply dummy text of the printing and typesetting. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,                           </div>
-                                        <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-3">
+                                        <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-5">
                                             <div>11:30</div>
                                             <div>21 January, 2021</div>
                                         </div>
@@ -180,7 +183,7 @@ export default function Messages() {
 
                                             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                                         </div>
-                                        <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-3">
+                                        <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-5">
                                             <div>9:30</div>
                                             <div>21 January, 2021</div>
                                         </div>
@@ -202,7 +205,7 @@ export default function Messages() {
                                             <div className="messages text-left text-black">
                                                 Lorem Ipsum is simply dummy text of the printing and typesetting factory. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it.
                                         </div>
-                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-3">
+                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-5">
                                                 <div>10:30</div>
                                                 <div>9 January, 2021</div>
                                             </div>
@@ -212,7 +215,7 @@ export default function Messages() {
                                         <div className="d-flex align-items-center justify-content-center mt-5">
                                             <div className="messages text-left text-black">
                                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,                           </div>
-                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-3">
+                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-5">
                                                 <div>6:30</div>
                                                 <div>10 January, 2021</div>
                                             </div>
@@ -225,7 +228,7 @@ export default function Messages() {
 
                                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                                        </div>
-                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-3">
+                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-5">
                                                 <div>11:30</div>
                                                 <div>22 January, 2021</div>
                                             </div>
@@ -236,7 +239,7 @@ export default function Messages() {
                                 </div>
 
                             </> : slectReject ?
-                                <><div className="d-flex flex-column justify-content-center chat-box">
+                                <><div className="d-flex flex-column justify-content-center  chat-box">
                                     <div className="message-header mb-auto">
                                         {slectWin ? "Win" : slectEnroll ? "Enrolled" : slectReject ? "Rejected" : slectTransfer ? "Transfer" : ""}
                                     </div>
@@ -245,7 +248,7 @@ export default function Messages() {
                                             <div className="messages text-left text-black">
                                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                                             </div>
-                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-3">
+                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-5">
                                                 <div>10:30</div>
                                                 <div>12 January, 2021</div>
                                             </div>
@@ -255,20 +258,37 @@ export default function Messages() {
                                         <div className="d-flex align-items-center justify-content-center mt-5">
                                             <div className="messages text-left text-black">
                                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,                           </div>
-                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-3">
+                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-5">
+                                                <div>10:30</div>
+                                                <div>12 January, 2021</div>
+                                            </div>
+
+                                        </div>
+                                        <div className="d-flex align-items-center justify-content-center mt-5">
+                                            <div className="messages text-left text-black">
+                                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,                           </div>
+                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-5">
+                                                <div>10:30</div>
+                                                <div>12 January, 2021</div>
+                                            </div>
+
+                                        </div>
+                                        <div className="d-flex align-items-center justify-content-center mt-5">
+                                            <div className="messages text-left text-black">
+                                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,                           </div>
+                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-5">
                                                 <div>10:30</div>
                                                 <div>12 January, 2021</div>
                                             </div>
 
                                         </div>
 
-
                                         <div className="d-flex align-items-center justify-content-center my-5">
                                             <div className="messages text-left text-black">
 
                                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                                            </div>
-                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-3">
+                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-5">
                                                 <div>10:30</div>
                                                 <div>12 January, 2021</div>
                                             </div>
@@ -287,7 +307,7 @@ export default function Messages() {
                                             <div className="messages text-left text-black">
                                                 Lorem Ipsum is simply dummy text of the printing. Lorem Ipsum has been the word factory's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                                            </div>
-                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-3">
+                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-5">
                                                 <div>9:30</div>
                                                 <div>1 January, 2021</div>
                                             </div>
@@ -297,7 +317,7 @@ export default function Messages() {
                                         <div className="d-flex align-items-center justify-content-center mt-5">
                                             <div className="messages text-left text-black">
                                                 Lorem Ipsum is simply dummy or sample text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,                           </div>
-                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-3">
+                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-5">
                                                 <div>22:30</div>
                                                 <div>12 January, 2021</div>
                                             </div>
@@ -310,7 +330,7 @@ export default function Messages() {
 
                                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                                             </div>
-                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-3">
+                                            <div className="time-stamp d-flex flex-column align-items-start justify-content-center ml-5">
                                                 <div>13:30</div>
                                                 <div>23 January, 2021</div>
                                             </div>
