5c1a8245c5660ff4ee6840392cabd9e2f0cdbdb0
Moneeza Syed <14besemsyed@seecs.edu.pk>
2021-02-21 19:42:39 +0500

    added some changes

--- src/pages/Stakes/Stakes.jsx
+++ src/pages/Stakes/Stakes.jsx
@@ -35,61 +35,104 @@ export default function Stakes() {
                     <div className="d-flex align-items-center justify-content-center flex-wrap">
                         {
                             leagues.map((league, value) => {
-                                return (<div key={league.id} className="d-flex flex-column justify-content-center align-items-flex-start div1-stake m-3">
-                                    <div className="child1-stake ml-1"></div>
-                                    <div className="child2-stake mx-3"></div>
-                                    <div className="text-left prize mt-3" style={{ fontSize: "0.5rem", fontFamily: "Rubik" }}>
-                                        {league.prize[0].label}
-                                    </div>
+                                if (value + 1 === leagues.length) {
+                                    return (
+                                        <div key={league.id} className="d-flex flex-column justify-content-center align-items-flex-start last-stake m-3">
+                                          <div className="child1-last-stake ml-1"></div>
+                                          <div className="child2-last-stake mx-3"></div>
+                                          <div className="last-stake-row mt-3">
+                                            <div className="d-flex flex-column justify-content-center mr-3">
+                                               <div className="text-left prize mt-3" style={{ fontSize: "0.5rem", fontFamily: "Rubik" }}>
+                                                 {league.prize[0].label}
+                                               </div>
+                                               <div className="text-left prize-amount mb-1">{league.prize[0].label}</div>
+                                             </div>
+                                             <div className="d-flex flex-column justify-content-center mr-3">
+                                                <div className="text-left prize" style={{ fontSize: "0.5rem", fontFamily: "Rubik" }}>
+                                                  {league.prize[1].label}</div>
+                                                <div className="text-left prize-amount mb-1">{league.prize[1].label}</div>
+                                             </div>
+                                             <div className="d-flex flex-column justify-content-center mr-3">
+                                                <div className="text-left prize" style={{ fontSize: "0.5rem", fontFamily: "Rubik" }}>
+                                                   {league.prize[2].label}</div>
+                                                <div className="text-left prize-amount mb-1">{league.prize[2].label}</div>
+                                             </div>
+                                          </div>
 
-                                    <div className="text-left prize-amount mb-1">{league.prize[0].label}</div>
-                                    <div className="text-left prize" style={{ fontSize: "0.5rem", fontFamily: "Rubik" }}>
-                                        {league.prize[1].label}</div>
-                                    <div className="text-left prize-amount mb-1">{league.prize[1].label}</div>
-                                    <div className="text-left prize" style={{ fontSize: "0.5rem", fontFamily: "Rubik" }}>
-                                        {league.prize[2].label}</div>
-                                    <div className="text-left prize-amount mb-1">{league.prize[2].label}
-                                    </div>
-
-                                    <div className="d-flex justify-content-between align-items-center text-white mt-3">
-                                        <div className="d-flex flex-column">
-                                            <div className="get-started">Get Started with</div>
-                                            <div className="prize-heading">{league.value}</div>
-                                        </div>
-                                        <div onClick={() => payStakes({ start: league.value, prize: league.prize[0].label, league_id: league.id })}>
-                                            <img src={ArrowRight} style={{
+                                          <div className="d-flex justify-content-between align-items-center text-white mt-3">
+                                            <div className="d-flex flex-column">
+                                              <div className="get-started">Get Started with</div>
+                                              <div className="prize-heading">{league.value}</div>
+                                            </div>
+                                            <div onClick={() => payStakes({ start: league.value, prize: league.prize[0].label, league_id: league.id })}
+                                             className="get-started-arrow">
+                                               <img src={ArrowRight} style={{
                                                 width: "4rem",
                                                 filter: "invert(96%) sepia(0%) saturate(0%) hue-rotate(111deg) brightness(104%) contrast(105%)"
+                                                }}/>
+                                            </div>
+                                          </div>
+                                        </div>
+                                )}
+                                else{
+                                    return(
+                                    return (
+                                        <div key={league.id} className="d-flex flex-column justify-content-center align-items-flex-start div1-stake m-3">
+                                          <div className="child1-stake ml-1"></div>
+                                          <div className="child2-stake mx-3"></div>
+                                          <div className="text-left prize mt-3" style={{ fontSize: "0.5rem", fontFamily: "Rubik" }}>
+                                                 {league.prize[0].label}
+                                          </div>
+                                          <div className="text-left prize-amount mb-1">{league.prize[0].label}</div>
+                                          <div className="text-left prize" style={{ fontSize: "0.5rem", fontFamily: "Rubik" }}>
+                                                 {league.prize[1].label}
+                                          </div>
+                                          <div className="text-left prize-amount mb-1">{league.prize[1].label}</div>
+                                           <div className="text-left prize" style={{ fontSize: "0.5rem", fontFamily: "Rubik" }}>
+                                                 {league.prize[2].label}</div>
+                                           <div className="text-left prize-amount mb-1">{league.prize[2].label}</div>
+                                          <div className="d-flex justify-content-between align-items-center text-white mt-3">
+                                            <div className="d-flex flex-column">
+                                              <div className="get-started">Get Started with</div>
+                                              <div className="prize-heading">{league.value}</div>
+                                            </div>
+                                            <div onClick={() => payStakes({ start: league.value, prize: league.prize[0].label, league_id: league.id })}
+                                             className="arrow">
+                                               <img src={ArrowRight} style={{
+                                                width: "4rem",
+                                                filter: "invert(96%) sepia(0%) saturate(0%) hue-rotate(111deg) brightness(104%) contrast(105%)"
+                                                }}/>
+                                            </div>
+                                          </div>
+                                        </div>
+                                    )
+                                }
 
-                                            }}  />
-                                    </div></div>
-                            </div>
                             )}
                         )}
 
                     </div>
-
                 </div>
+               <div className="d-flex justify-content-center align-items-center m-5">
+                   <Link to={Pdf} target="_blank"><button style={{
+                        borderRadius: "17px",
+                        /* padding: 0.5rem; */
+                        fontSize: "20px",
+                        fontWeight: "600",
+                        fontFamily: "Rubik",
+                        color: "#492477",
+                        backgroundColor: "white",
+                        boxShadow: "0px 3px 6px #00000029",
+                        border: "1px solid #492477",
+                        padding: "1rem 2rem"
+                        }}>
+                       See What you can win
+                      </button>
+                   </Link>
+               </div>
             </div>
-            <div className="d-flex justify-content-center align-items-center m-5">
-                <Link to="/winnings"><button style={{
-                    borderRadius: "26px",
-                    /* padding: 0.5rem; */
-                    fontSize: "20px",
-                    fontWeight: "600",
-                    color: "#492477",
-                    backgroundColor: "white",
-                    boxShadow: "0px 3px 6px #00000029",
-                    border: "1px solid #492477",
-                    padding: "1rem 2rem"
-                }}>
-                    See What you can win
-                   </button>
-                </Link>
-            </div>
-        </div>
-
-        <SubscribeLetter />
+        </div>  
+       <SubscribeLetter />
     </>)
 
 }
